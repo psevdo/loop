36ce35c4557ce2848c7c3ff049eab72d20f7ca83
psevdo <psevdo85@gmail.com>
2017-07-19 08:16:43 +0300

    go

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+class Loop {
+	private $amount = 0;
+	
+	public function amountSet($amount) {
+		$this->amount = $amount;
+	}
+	
+	public function amountGet() {
+		return $this->amount;
+	}
+	
+	public function startLoop() {
+		if($this->amount <= 0)
+			return;
+		
+		for($i = 0; $i< $this->amount; $i++)
+			echo $i.'<br />';
+	}
+	
+}
